72afe9e0bea565814a11ffba9c4853c557adea3a
Pedro Haramoto <pedroharamoto@gmail.com>
2023-10-31 22:10:02 -0300

    infra VPC basica

--- main.tf
+++ main.tf
@@ -2,8 +2,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "vpc-soat" {
-  cidr_block           = "10.0.0.0/26"
+### VPC CONFIG ###
+
+resource "aws_vpc" "vpc_soat" {
+  cidr_block           = "10.0.0.0/23"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
@@ -12,9 +14,21 @@ resource "aws_vpc" "vpc-soat" {
   }
 }
 
-resource "aws_subnet" "soat-subnet-private1-us-east-1a" {
-  vpc_id                  = aws_vpc.vpc-soat.id
-  cidr_block              = "10.0.0.0/28"
+output "vpc_soat_id" {
+  value = aws_vpc.vpc_soat.id
+}
+
+resource "aws_route_table" "route_table_a" {
+  vpc_id = aws_vpc.vpc_soat.id
+}
+
+resource "aws_route_table" "route_table_b" {
+  vpc_id = aws_vpc.vpc_soat.id
+}
+
+resource "aws_subnet" "soat_subnet_private1_us_east_1a" {
+  vpc_id                  = aws_vpc.vpc_soat.id
+  cidr_block              = "10.0.0.0/24"
   availability_zone       = var.aws_az_a
   map_public_ip_on_launch = false
   tags = {
@@ -23,9 +37,22 @@ resource "aws_subnet" "soat-subnet-private1-us-east-1a" {
   }
 }
 
-resource "aws_subnet" "soat-subnet-private1-us-east-1b" {
-  vpc_id                  = aws_vpc.vpc-soat.id
-  cidr_block              = "10.0.0.16/28"
+output "subnet_a_id" {
+  value = aws_subnet.soat_subnet_private1_us_east_1a.id
+}
+
+resource "aws_route_table_association" "subnet_association_a" {
+  subnet_id      = aws_subnet.soat_subnet_private1_us_east_1a.id
+  route_table_id = aws_route_table.route_table_a.id
+}
+
+output "route_table_a" {
+  value = aws_route_table_association.subnet_association_a.id
+}
+
+resource "aws_subnet" "soat_subnet_private1_us_east_1b" {
+  vpc_id                  = aws_vpc.vpc_soat.id
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = var.aws_az_b
   map_public_ip_on_launch = false
   tags = {
@@ -34,14 +61,27 @@ resource "aws_subnet" "soat-subnet-private1-us-east-1b" {
   }
 }
 
-resource "aws_security_group" "security-group-load-balancer" {
+output "subnet_b_id" {
+  value = aws_subnet.soat_subnet_private1_us_east_1b.id
+}
+
+resource "aws_route_table_association" "subnet_association_b" {
+  subnet_id      = aws_subnet.soat_subnet_private1_us_east_1b.id
+  route_table_id = aws_route_table.route_table_b.id
+}
+
+output "route_table_b" {
+  value = aws_route_table_association.subnet_association_b.id
+}
+
+resource "aws_security_group" "security_group_load_balancer" {
   name_prefix = "security-group-load-balancer"
   description = "load balancer SG"
-  vpc_id      = aws_vpc.vpc-soat.id
+  vpc_id      = aws_vpc.vpc_soat.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -59,16 +99,16 @@ resource "aws_security_group" "security-group-load-balancer" {
   }
 }
 
-resource "aws_security_group" "security-group-cluster-ecs" {
+resource "aws_security_group" "security_group_cluster_ecs" {
   name_prefix = "security-group-cluster-ecs"
   description = "cluster ecs SG"
-  vpc_id      = aws_vpc.vpc-soat.id
+  vpc_id      = aws_vpc.vpc_soat.id
 
   ingress {
     from_port       = 8080
     to_port         = 8080
     protocol        = "tcp"
-    security_groups = [aws_security_group.security-group-load-balancer.id]
+    security_groups = [aws_security_group.security_group_load_balancer.id]
   }
 
   egress {
@@ -82,4 +122,139 @@ resource "aws_security_group" "security-group-cluster-ecs" {
     infra = "vpc-soat"
     Name  = "security-group-cluster-ecs"
   }
+}
+
+output "security_group_ecs_id" {
+  value = aws_security_group.security_group_cluster_ecs.id
+}
+
+resource "aws_security_group_rule" "security_group_cluster_ecs_ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.security_group_cluster_ecs.id
+  depends_on        = [aws_ecs_cluster.cluster_ecs_soat]
+}
+
+
+
+### Target Group + Load Balancer
+
+resource "aws_lb_target_group" "target_group_soat_api" {
+  depends_on = [ aws_vpc.vpc_soat ]
+  name        = "tg-soat-api"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.vpc_soat.id
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    matcher             = "200-299"
+    path                = "/health_check"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    infra = "tg-soat-api"
+  }
+}
+
+output "target_group_soat_api_arn" {
+  value = aws_lb_target_group.target_group_soat_api.arn
+}
+
+resource "aws_lb" "alb_soat_api" {
+  name               = "alb-soat-api"
+  internal           = true
+  load_balancer_type = "application"
+  ip_address_type    = "ipv4"
+
+  security_groups = [aws_security_group.security_group_load_balancer.id]
+  subnets = [
+    aws_subnet.soat_subnet_private1_us_east_1a.id,
+    aws_subnet.soat_subnet_private1_us_east_1b.id
+  ]
+
+  tags = {
+    infra = "alb-soat-api"
+  }
+}
+
+resource "aws_lb_listener" "alb_soat_listener" {
+  load_balancer_arn = aws_lb.alb_soat_api.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group_soat_api.arn
+  }
+
+  tags = {
+    Name  = "alb-soat-listener"
+    infra = "alb-soat-listener"
+  }
+
+}
+
+### ECS CONFIG ###
+
+resource "aws_ecs_cluster" "cluster_ecs_soat" {
+  name = "cluster-ecs-soat"
+
+  tags = {
+    infra = "cluster-ecs-soat"
+  }
+}
+
+output "cluster_ecs_soat_id" {
+  value = aws_ecs_cluster.cluster_ecs_soat.id
+}
+
+resource "aws_ecs_task_definition" "task_definition_ecs" {
+  family                   = "task-definition-family-01"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  execution_role_arn       = var.execution_role_ecs
+
+  cpu    = 512
+  memory = 1024
+  container_definitions = jsonencode([
+    {
+      name      = "container-1"
+      image     = var.ecr_image
+      cpu       = 256,
+      memory    = 256,
+      essential = true,
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
+          appProtocol   = "http"
+        }
+      ]
+    }
+  ])
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  tags = {
+    infra = "task-definition-ecs"
+  }
+}
+
+output "task_definition_ecs_arn" {
+  value = aws_ecs_task_definition.task_definition_ecs.arn
 }
--- variables.tf
+++ variables.tf
@@ -13,4 +13,17 @@ variable "aws_az_b" {
   default = "us-east-1b"
 }
 
+variable "ecr_image" { 
+  type = string
+  default = ""
+}
 
+variable "execution_role_ecs" { 
+  type = string
+  default = ""
+}
+
+variable "desired_tasks" { 
+  type = number
+  default = 1
+}
